cd218f22e7f12d9aab54fa5b112bf6d4ff60d9e8
Igor <igor53c@gmail.com>
2023-07-25 19:19:21 +0200

    Add project files.

---
+++ CollatzConjecture/CollatzChallengeTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Xunit;
+using CollatzChallengeImplementation;
+
+namespace CollatzChallengeTests
+{
+    public class CollatzConjectureTests
+    {
+        [Fact]
+        public void TestPositiveNumber()
+        {
+            // Arrange
+            int number = 20;
+            int expectedSteps = 7;
+
+            // Act
+            int actualSteps = CollatzChallengeImplementation.CollatzConjecture.CountStepsToOne(number);
+
+            // Assert
+            Assert.Equal(expectedSteps, actualSteps);
+        }
+
+        [Fact]
+        public void TestZero()
+        {
+            // Arrange
+            int number = 0;
+
+            // Act and Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => CollatzChallengeImplementation.CollatzConjecture.CountStepsToOne(number));
+        }
+
+        [Fact]
+        public void TestNegativeNumber()
+        {
+            // Arrange
+            int number = -10;
+
+            // Act and Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => CollatzChallengeImplementation.CollatzConjecture.CountStepsToOne(number));
+        }
+
+        [Fact]
+        public void TestOne()
+        {
+            int number = 1;
+            int expectedSteps = 0;
+
+            int actualSteps = CollatzChallengeImplementation.CollatzConjecture.CountStepsToOne(number);
+
+            Assert.Equal(expectedSteps, actualSteps);
+        }
+
+        [Fact]
+        public void TestSmallNumber()
+        {
+            int number = 2;
+            int expectedSteps = 1;
+
+            int actualSteps = CollatzChallengeImplementation.CollatzConjecture.CountStepsToOne(number);
+
+            Assert.Equal(expectedSteps, actualSteps);
+        }
+
+        [Fact]
+        public void TestLargeNumber()
+        {
+            int number = 1000000;
+            // This value is known from the problem, it is not recommended to hardcode expected results
+            int expectedSteps = 152;
+
+            int actualSteps = CollatzChallengeImplementation.CollatzConjecture.CountStepsToOne(number);
+
+            Assert.Equal(expectedSteps, actualSteps);
+        }
+    }
+}
---
+++ CollatzConjecture/CollatzConjecture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollatzChallengeImplementation
+{
+    public static class CollatzConjecture
+    {
+        public static int CountStepsToOne(int number)
+        {
+            if (number <= 0)
+                throw new ArgumentOutOfRangeException("Number must be a positive integer greater than 0.");
+
+            int steps = 0;
+            while (number != 1)
+            {
+                if (number % 2 == 0)
+                {
+                    number /= 2;
+                }
+                else
+                {
+                    number = number * 3 + 1;
+                }
+                steps++;
+            }
+            return steps;
+        }
+    }
+}
